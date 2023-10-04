8d465d90ad9d0a4356b1adbafbf3c36d86527864
aaronmoore1217 <126786373+aaronmoore1217@users.noreply.github.com>
2023-10-04 13:18:17 -0400

    player developemtn beings

---
+++ Scripts/Player/playerCharacter.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class playerCharacter : CharacterBody2D
+{
+    public int Speed { get; set; } = 400;
+    public Vector2 ScreenSize;
+
+    public override void _Ready(){
+        ScreenSize = GetViewportRect().Size;
+    }
+    public override void _Process(double delta){
+
+    }
+}
