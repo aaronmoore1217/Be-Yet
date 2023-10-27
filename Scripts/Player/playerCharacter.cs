061176aa234e068715879fd5e561a5aaf900919d
aaronmoore1217 <126786373+aaronmoore1217@users.noreply.github.com>
2023-10-27 14:13:14 -0400

    rahhh

--- Scripts/Player/playerCharacter.cs
+++ Scripts/Player/playerCharacter.cs
@@ -5,16 +5,47 @@ public partial class playerCharacter : CharacterBody2D
 {
     public int Speed { get; set; } = 400;
     public bool turnedRight = true;
+    AnimatedSprite2D playerAnimation;
     //runs on game ready
     public override void _Ready()
     {
-
+        playerAnimation = GetNode<AnimatedSprite2D>("playerAnimation");
     }
 
     public void GetInput()
     {
         Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
         Velocity = inputDirection * Speed;
+        
+        if (Input.IsActionPressed("right"))
+        {
+            turnedRight = true;
+        }
+        if (Input.IsActionPressed("left"))
+        {
+            turnedRight = false;
+        }
+
+        //start and stop animation
+        if (Velocity.Length() > 0)
+        {
+            Velocity = Velocity.Normalized() * Speed;
+            playerAnimation.Play();
+        }
+        else
+        {
+            playerAnimation.Stop();
+        }
+
+        //turning
+        if (turnedRight)
+        {
+            playerAnimation.FlipH = false;
+        }
+        else
+        {
+            playerAnimation.FlipH = true;
+        }
     }
 
     public override void _PhysicsProcess(double delta)
@@ -25,57 +56,6 @@ public partial class playerCharacter : CharacterBody2D
 
     public override void _Process(double delta)
     {
-
-        Vector2 position = Vector2.Zero; // The player's movement vector.
-        var playerAnimation = GetNode<AnimatedSprite2D>("playerAnimation");
-
-        /* movement */
-        {
-            if (Input.IsActionPressed("right"))
-            {
-                turnedRight = true;
-            }
-
-            if (Input.IsActionPressed("left"))
-            {
-                turnedRight = false;
-            }
-
-            
-        }
-
-        if (turnedRight)
-        {
-            playerAnimation.FlipH = false;
-        }
-        else
-        {
-            playerAnimation.FlipH = true;
-        }
-
-        if (position.Length() > 0)
-        {
-            position = position.Normalized() * Speed;
-            playerAnimation.Play();
-        }
-        else
-        {
-            playerAnimation.Stop();
-        }
-
-        Position += position * (float)delta;
-        // simple way to flip animation, too simple, need to keep direction.
-        // if (position.X < 0)
-        // {
-        //     playerAnimation.FlipH = true;
-        // }
-        // else
-        // {
-        //     playerAnimation.FlipH = false;
-        // }
-        // Position = new Vector2(
-        //     x: Mathf.Clamp(Position.X, 0, ScreenSize.X-106),
-        //     y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y-106)
-        // );
+        
     }
 }
